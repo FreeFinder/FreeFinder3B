30062d4cc4aea1d63f799827db24df653096ed3f
Charles Gravitt <cbgravitt@uchicago.edu>
2022-11-07 21:00:52 -0600

    'item class etc'

---
+++ FreeFinders3B/main.swift
@@ -0,0 +1,96 @@
+//
+//  main.swift
+//  FreeFinderMilestone3B
+//
+//  Created by Charlie Gravitt on 11/2/22.
+//
+
+import SwiftUI
+import GoogleSignIn
+import MapKit
+
+func refresh(){
+    
+}
+
+func item_to_annot(item_id: String){
+    
+}
+
+func sign_in() -> User?{
+    
+    GIDSignIn.sharedInstance().restorePreviousSignIn()
+    var u = GIDSignIn.sharedInstance().currentUser
+    if u != nil{
+        let mail = u?.profile.email
+        let ID = u?.userID
+        return User(ID: ID, mail: mail)
+    }
+    GIDSignIn.sharedInstance()?.signIn()
+    u = GIDSignIn.sharedInstance().currentUser
+    if u != nil{
+        let mail = u?.profile.email
+        let ID = u?.userID
+        //TODO: add to DB
+        return User(ID: ID, mail: mail)
+    }else{
+        return nil
+    }
+}
+
+class User {
+    var id : String?
+    var email : String?
+
+    init(ID: String?, mail: String?){
+        self.id = ID
+        self.email = mail
+    }
+    
+    func create_item(){
+        
+    }
+    
+    func comment(item_id: String, comment: String)-> Bool{
+        return true
+    }
+    
+    func delete_item(item_id: String){
+        
+    }
+    
+    func sign_out() -> Bool{
+        GIDSignIn.sharedInstance()?.signOut()
+        return (GIDSignIn.sharedInstance() == nil)
+    }
+    
+}
+
+class Item: NSObject, MKAnnotation{
+    let name: String?
+    let type: String?
+    let coordinate: CLLocationCoordinate2D
+    let comments: [String]
+    private let creator: User
+    let counter: Int
+    
+    init(name: String?, type: String?, coordinate: CLLocationCoordinate2D, creator: User){
+        self.name = name
+        self.type = type
+        self.coordinate = coordinate
+        self.creator = creator
+        self.comments = []
+        self.counter = 0
+        
+        super.init()
+    }
+    
+    var title: String?{
+        return name
+    }
+    
+    var subtitle: String?{
+        return type
+    }
+}
+
