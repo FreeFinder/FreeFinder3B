e8b03172503047b70b7eab8a0a01a2cdb3fe5529
steven arellano <stevenjarellano2@gmail.com>
2022-11-08 22:15:52 -0600

    new refresh function

--- FreeFinders3B/Classes/Item.swift
+++ FreeFinders3B/Classes/Item.swift
@@ -171,7 +171,7 @@ class Item: NSObject, MKAnnotation{
             if (await db_item_exists()) {
                 self.comments.append(comment)
                 self.db_add_comment(comment: comment)
-                await observer.refresh()
+                await refresh()
                 return true
             }
             else{
@@ -184,7 +184,7 @@ class Item: NSObject, MKAnnotation{
         //deletes if item in database 
         if (await self.db_item_exists()){
             await db_delete_item();
-            await observer.refresh();
+            await refresh();
         }
     }
 }
---
+++ FreeFinders3B/utilities.swift
@@ -0,0 +1,16 @@
+//
+//  utilities.swift
+//  FreeFinders3B
+//
+//  Created by steven arellano on 11/8/22.
+//
+
+import Foundation
+
+
+func refresh() async -> [Item] {
+    let user = await User(email: "mongodb@gmail.com");
+    let observer = await AppData(user: user);
+    
+    return await observer.db_get_all_items();
+}
