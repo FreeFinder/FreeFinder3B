53ca55cd0e45a1c042969e0e2a85505b7b9e7b40
wzeng0 <wzeng00@uchicago.edu>
2022-11-08 18:51:47 -0600

    removed google dependency and pods'

--- Pods/Target Support Files/Pods-FreeFinders3B-FreeFinders3BUITests/Pods-FreeFinders3B-FreeFinders3BUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FreeFinders3B_FreeFinders3BUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FreeFinders3B_FreeFinders3BUITests
-@end
--- Pods/Target Support Files/Pods-FreeFinders3BTests/Pods-FreeFinders3BTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FreeFinders3BTests : NSObject
-@end
-@implementation PodsDummy_Pods_FreeFinders3BTests
-@end
