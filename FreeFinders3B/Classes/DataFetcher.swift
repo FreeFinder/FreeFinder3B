4b1865555b2ae4d59db21f2a1cd9a4f45c538568
steven arellano <stevenjarellano2@gmail.com>
2022-11-08 18:06:41 -0600

    user finished + appdata fixes

--- FreeFinders3B/Classes/DataFetcher.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DataFetcher.swift
-//  FreeFinders3B
-//
-//  Created by steven arellano on 11/8/22.
-//
-
-import Foundation
--- FreeFinders3B/Classes/User.swift
+++ FreeFinders3B/Classes/User.swift
@@ -9,39 +9,52 @@ import Foundation
 import SwiftUI
 import GoogleSignIn
 import MapKit
+import RealmSwift
 
 class User {
-    var id : String?
-    var email : String?
+    var id : ObjectId
+    var email : String
 
-    init(ID: String?, mail: String?){
-        self.id = ID
-        self.email = mail
-        db_add_user()
+    init(email: String, id: ObjectId = ObjectId()) async {
+        self.email = email;
+        self.id = id;
+        
+        if (id == ObjectId()) {
+            await db_add_user(email: email);
+        }
     }
     
-    private func db_add_user(){
-        // add user, user might already exist (depends on sign in sign out stuff)
-    }
-    
-    func db_format_user_as_creator(){
-        // return however it needs formatting for writing item creator to db
-    }
+    func db_add_user(email: String) async {
+        do {
+            let app = App(id: APP_ID);
+            let mongo_user = try await app.login(credentials: Credentials.anonymous);
+            // fetch the DB
+            let client = app.currentUser!.mongoClient("mongodb-atlas")
+            let database = client.database(named: "freeFinder")
+            let users = database.collection(withName: "users")
             
-    func create_item(name: String?, type: String?, detail: String?, coordinate: CLLocationCoordinate2D) -> Bool{
-        // check field validity
-        _ = Item(name: name,type: type,detail: detail,coordinate: coordinate,creator: self)
-        refresh()
-        return true
+            let potentialUser: Document = ["email" : AnyBSON(stringLiteral: email)];
+            let user = try await users.findOneDocument(filter: potentialUser);
+            // if the user is in the DB already
+            if (user != nil) {
+                print("Account with this email found in the database: \(String(describing: user))");
+                self.id = ((user?["_id"]!!)?.objectIdValue!)!;
+                return;
+            }
+            // if the user is not currently in the db
+            print("Email is not currently registered in the database, creating an account now.");
+            let newUserId = try await users.insertOne(potentialUser);
+            print("New Account created under user id: \(newUserId)");
+            self.id = newUserId.objectIdValue!;
+            
+        } catch {
+            print("Failed to add/retrieve this user from the database")
+        }
     }
     
-    func comment(i: Item, comment: String)-> Bool{
-        return i.add_Comment(comment: comment)
+    func comment(i: Item, comment: String) async -> Bool{
+        return await i.add_Comment(comment: comment)
 
-    }
-    
-    func delete_item(i: Item) -> Bool{
-        return i.delete_Item()
     }
     
     func sign_out() -> Bool{
